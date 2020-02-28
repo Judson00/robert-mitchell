17cf1f3c51d02a6cb061dcce63456e2f40af33da
Robert J. Mitchell <robert.judson@outlook.com>
2020-02-27 22:04:17 -0500

    Nav finished. Working on Home Page now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import '../styles/app.css';
 import NavHeader from './NavHeader';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
       <NavHeader />
+      <HomePage />
     </div>
   );
 }
